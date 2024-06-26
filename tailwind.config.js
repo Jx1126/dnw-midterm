de7137b1bf03c69387111b5d1e2a4ff3e73e3036
Wong Jin Xuan <jinxuan03@gmail.com>
2024-06-26 15:40:38 +0800

    Modified button CSS and modified author home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./views/*.{html,js,ejs}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#414A5B',
+        'secondary-color': '#6B7280',
+        'secondary-hover-color': '#7d8296'
+      }
+    },
   },
   plugins: [require('daisyui'),],
   daisyui: {
