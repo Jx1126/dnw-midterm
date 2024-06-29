bfc92195f27d7b94640542abbb72394152116365
Wong Jin Xuan <jinxuan03@gmail.com>
2024-06-29 22:34:56 +0800

    Created component layout for header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/*.{html,js,ejs}'],
+  content: ['./views/*.{html,js,ejs}', './views/layout/*.{html,js,ejs}'],
   theme: {
     extend: {
       colors: {
