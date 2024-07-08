5ed9c6946e71865c47a6d7c013ab0a161cfe381b
Wong Jin Xuan <jinxuan03@gmail.com>
2024-07-08 16:15:19 +0800

    Comments on published articles are now supported Bug: - Comments seem to save, but doesnt appear in the database

--- routes/reader_articles.js
+++ routes/reader_articles.js
@@ -33,17 +33,31 @@ router.get('/article', (req, res) => {
     });
   });
 
-  Promise.all([getBlogInformation, getPublishedArticles])
-    .then(([getBlogInformation, getPublishedArticles]) => {
-      let obj = { author: getBlogInformation, published: getPublishedArticles }
+  const getComments = new Promise((resolve, reject) => {
+    // const sql = `SELECT * FROM Comments WHERE article_id = '${req.query.id}';`;
+    // To sort by the creation date of the comments, the line above should be modified into:
+    const sql = `SELECT * FROM Comments WHERE article_id = '${req.query.id}' ORDER BY creation DESC;`;
+    db.all(sql, (err, rows) => {
+      if (err) {
+        console.error('Error querying the database: ' + err.message);
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+
+  Promise.all([getBlogInformation, getPublishedArticles, getComments])
+    .then(([getBlogInformation, getPublishedArticles, getComments]) => {
+      let obj = { author: getBlogInformation, published: getPublishedArticles, comments: getComments}
       if (req.query.success) {
-        obj.success = 'Settings saved successfully.'
+        obj.success = 'Comment posted successfully.'
       }
       return res.render('reader_articles', obj);
     })
     .catch((err) => {
       console.error('Error querying the database: ' + err.message);
-      return res.render('reader_articles', { author: { author_name: 'Default Author', blog_title: 'Default Blog' }, success: req.query.success ? 'Settings saved successfully.' : null });
+      return res.render('reader_articles', { author: { author_name: 'Default Author', blog_title: 'Default Blog' }, success: req.query.success ? 'Comment posted successfully.' : null });
     });
 });
 
@@ -55,15 +69,25 @@ router.post('/article', urlencodedParser, [
     check('comment', 'Comment cannot be empty.').notEmpty(),
   ],
   (req, res) => {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()) {
-          const alert = errors.array()
-          res.render('reader_articles', {
-              alert
-          })
-      }else{
-          return res.redirect('/reader/article?success=1')
-      }
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        const alert = errors.array()
+        res.render('reader_articles', {
+            alert,
+            published: {}
+        })
+    }else{
+      const { commenter_name, comment } = req.body;
+      const article_id = req.query.id;
+      const sql = `INSERT INTO Comments (article_id, commenter, comment) VALUES (?, ?, ?)`;
+      db.run(sql, [article_id, commenter_name, comment], (err) => {
+        if (err) {
+          console.error('Error posting comment: ' + err.message);
+          return res.status(500).send('Error posting comment');
+        }
+      });
+      return res.redirect(`/reader/article?id=${article_id}&success=1`)
+    }
   }
 ])
 
