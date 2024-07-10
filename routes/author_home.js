e9f2474d6d570d3d403b477b1c681eab23e0debc
Wong Jin Xuan <jinxuan03@gmail.com>
2024-07-10 16:13:55 +0800

    Remove some redundant comments

--- routes/author_home.js
+++ routes/author_home.js
@@ -79,14 +79,6 @@ router.get('/publish', (req, res) => {
 });
 
 router.get('/delete', (req, res) => {
-  // const sql = `DELETE FROM Articles WHERE id = '${req.query.id}'`;
-  // db.run(sql, (err) => {
-  //   if (err) {
-  //     console.error('Error deleting the article: ' + err.message);
-  //   }
-  //   return res.redirect('/author/home?deleted=true');
-  // });
-
   const removeComments = `DELETE FROM Comments WHERE article_id = '${req.query.id}'`;
   db.run(removeComments, (err) => {
     if (err) {
